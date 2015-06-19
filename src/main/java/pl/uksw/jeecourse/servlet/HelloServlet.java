b7b11ed18f10620cb789533e4ec73abfd22eff90
student <krzysztof.konwisarz@gmail.com>
2015-06-19 21:03:58 +0200

    Initial setup - base for the lab exercise

---
+++ src/main/java/pl/uksw/jeecourse/servlet/HelloServlet.java
@@ -0,0 +1,24 @@
+package pl.uksw.jeecourse.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Servlet HelloServlet at >>" + request.getContextPath() + "<<");
+        out.println("</body>");
+        out.println("</html>");
+    };
+
+}
